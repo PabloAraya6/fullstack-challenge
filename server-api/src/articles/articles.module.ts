0459d9111cc8b666a3ebf5fa9a9a9b9f91cb73e7
Pablo <paraya@folcode.com>
2021-11-12 02:31:25 -0300

    articles-seed done

--- server-api/src/articles/articles.module.ts
+++ server-api/src/articles/articles.module.ts
@@ -2,13 +2,17 @@ import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
 import { ArticlesController } from './articles.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ScheduleModule } from '@nestjs/schedule';
+import { HttpModule } from '@nestjs/axios';
 import { ArticleSchema, Article } from './schemas/article.schema';
-
+import { ArticlesSeedService } from './articles-seed.service';
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Article.name, schema: ArticleSchema }]),
+    HttpModule,
+    ScheduleModule.forRoot(),
   ],
-  providers: [ArticlesService],
   controllers: [ArticlesController],
+  providers: [ArticlesService, ArticlesSeedService],
 })
 export class ArticlesModule {}
